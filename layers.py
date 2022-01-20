30bcdcf21caee564729aa956bdf9a6411126ca63
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2022-01-20 15:54:23 +0700

    A little change

--- layers.py
+++ layers.py
@@ -34,8 +34,8 @@ class GAT_gate(torch.nn.Module):
         z = h
         for _ in range(self.nhop):
             az = F.relu(torch.einsum('aij,ajk->aik',(attention, z)))
-            coeff = torch.sigmoid(self.gate(torch.cat([x,az], -1))).repeat(1,1,x.size(-1))
-            z = coeff * x + (1 - coeff) * az
+            coeff = torch.sigmoid(self.gate(torch.cat([h, az], -1))).repeat(1, 1, h.size(-1))
+            z = coeff * h + (1 - coeff) * az
 
         if get_attention:
             return z, attention
