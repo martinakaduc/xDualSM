e02362c70d9951736b9db7d5b862362950137a0f
Martin Nguyen <nguyenquangduc2000@gmail.com>
2023-07-10 16:29:07 +0700

    Update for visualizing graph alignment

--- viz_matching.py
+++ viz_matching.py
@@ -6,16 +6,9 @@ import networkx as nx
 import numpy as np
 import torch
 import utils
+from dataset import onehot_encoding_node
 from gnn import gnn
 from scipy.spatial import distance_matrix
-
-
-def onehot_encoding_node(m, embedding_dim):
-    H = []
-    for i in m.nodes:
-        H.append(utils.node_feature(m, i, embedding_dim))
-    H = np.array(H)
-    return H
 
 
 class InferenceGNN:
@@ -25,9 +18,11 @@ class InferenceGNN:
             os.environ["CUDA_VISIBLE_DEVICES"] = cmd[:-1]
 
         self.model = gnn(args)
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = utils.initialize_model(
-            self.model, self.device, load_save_file=args.ckpt, gpu=(args.ngpu > 0)
+            self.model, self.device, load_save_file=args.ckpt, gpu=(
+                args.ngpu > 0)
         )
 
         self.model.eval()
@@ -73,7 +68,8 @@ class InferenceGNN:
         return sample
 
     def input_to_tensor(self, batch_input):
-        max_natoms = max([len(item["H"]) for item in batch_input if item is not None])
+        max_natoms = max([len(item["H"])
+                         for item in batch_input if item is not None])
         batch_size = len(batch_input)
 
         H = np.zeros((batch_size, max_natoms, batch_input[0]["H"].shape[-1]))
@@ -114,13 +110,13 @@ class InferenceGNN:
         list_inputs = self.prepare_multi_input(list_subgraphs, list_graphs)
         input_tensors = self.input_to_tensor(list_inputs)
         results = self.model.test_model(input_tensors)
-        return results.cpu().detach().numpy()
+        return results
 
     def predict_embedding(self, list_subgraphs, list_graphs):
         list_inputs = self.prepare_multi_input(list_subgraphs, list_graphs)
         input_tensors = self.input_to_tensor(list_inputs)
         results = self.model.get_refined_adjs2(input_tensors)
-        return results.cpu().detach().numpy()
+        return results
         # from scipy.spatial import distance_matrix
         # results = results.cpu().detach().numpy()
         # return [distance_matrix(results[0], results[0])]
@@ -133,7 +129,11 @@ if __name__ == "__main__":
         "-c",
         help="checkpoint for gnn",
         type=str,
-        default="model/best_large_30_20.pt",
+        default="",
+    )
+    parser.add_argument("--dataset", help="dataset", type=str, default="tiny")
+    parser.add_argument(
+        "--num_workers", help="number of workers", type=int, default=os.cpu_count()
     )
     parser.add_argument(
         "--confidence", help="isomorphism threshold", type=float, default=0.5
@@ -142,7 +142,8 @@ if __name__ == "__main__":
         "--mapping_threshold", help="mapping threshold", type=float, default=1e-5
     )
     parser.add_argument("--ngpu", help="number of gpu", type=int, default=1)
-    parser.add_argument("--batch_size", help="batch_size", type=int, default=32)
+    parser.add_argument("--batch_size", help="batch_size",
+                        type=int, default=32)
     parser.add_argument(
         "--embedding_dim",
         help="node embedding dim aka number of distinct node label",
@@ -155,52 +156,91 @@ if __name__ == "__main__":
     parser.add_argument(
         "--d_graph_layer", help="dimension of GNN layer", type=int, default=140
     )
-    parser.add_argument("--n_FC_layer", help="number of FC layer", type=int, default=4)
+    parser.add_argument(
+        "--n_FC_layer", help="number of FC layer", type=int, default=4)
     parser.add_argument(
         "--d_FC_layer", help="dimension of FC layer", type=int, default=128
     )
-    parser.add_argument("--dropout_rate", help="dropout_rate", type=float, default=0.0)
-    parser.add_argument("--al_scale", help="attn_loss scale", type=float, default=1.0)
+    parser.add_argument("--dropout_rate", help="dropout_rate",
+                        type=float, default=0.0)
+    parser.add_argument("--al_scale", help="attn_loss scale",
+                        type=float, default=1.0)
     parser.add_argument(
         "--tatic",
         help="tactic of defining number of hops",
         type=str,
         default="static",
-        choices=["static", "continuos", "jump"],
+        choices=["static", "cont", "jump"],
     )
     parser.add_argument("--nhop", help="number of hops", type=int, default=1)
+    parser.add_argument("--branch", help="choosing branch",
+                        type=str, default="both", choices=["both", "left", "right"])
+    parser.add_argument(
+        "--data_path", help="path to the data", type=str, default="data_processed"
+    )
+    parser.add_argument(
+        "--result_dir",
+        help="save directory of model parameter",
+        type=str,
+        default="results/",
+    )
+    parser.add_argument(
+        "--source", help="source graph idx", type=int, default=0
+    )
+    parser.add_argument(
+        "--query", help="query graph idx", type=int, default=0
+    )
+    parser.add_argument(
+        "--synthesis", action="store_true", help="synthesis data"
+    )
 
     args = parser.parse_args()
     print(args)
 
-    inference_gnn = InferenceGNN(args)
+    ngpu = args.ngpu
+    batch_size = args.batch_size
+    data_path = os.path.join(args.data_path, args.dataset)
+    result_dir = os.path.join(
+        args.result_dir, "%s_%s_%d" % (args.dataset, args.tatic, args.nhop)
+    )
+    if args.branch != "both":
+        result_dir += "_" + args.branch
+    if args.dataset not in args.ckpt:
+        cross_ckpt = args.ckpt.split("/")[1].split("_")[0]
+        result_dir += "_" + cross_ckpt
+    args.result_dir = result_dir
+
+    if not os.path.isdir(result_dir):
+        os.system("mkdir " + result_dir)
+
+    model = InferenceGNN(args)
 
     # Load subgraph
     subgraphs = utils.read_graphs(
-        "data_synthesis/datasets/tiny_30_20/7/iso_subgraphs.lg"
-    )
-    subgraph = subgraphs[3]
-    print("subgraph", subgraph is not None)
-    utils.plotGraph(subgraph, showLabel=False)
+        f"{data_path}/{args.source}/iso_subgraphs.lg")
+    subgraph = subgraphs[args.query]
+    print("subgraph", subgraph != None)
+    # utils.plotGraph(subgraph, showLabel=False)
 
     # Load graph
-    graphs = utils.read_graphs("data_synthesis/datasets/tiny_30_20/7/source.lg")
-    graph = graphs[7]
-    print("graph", graph is not None)
+    graphs = utils.read_graphs(
+        f"{data_path}/{args.source}/source.lg")
+    graph = graphs[0]
+    print("graph", graph != None)
     # utils.plotGraph(graph, showLabel=True)
 
     # Load mapping groundtruth
-    mapping_gt = utils.read_mapping(
-        "data_synthesis/datasets/tiny_30_20/7/iso_subgraphs_mapping.lg"
-    )[3]
+    mapping_gts = utils.read_mapping(
+        f"{data_path}/{args.source}/iso_subgraphs_mapping.lg")
+    mapping_gt = mapping_gts[args.query]
     print(mapping_gt)
 
-    results = inference_gnn.predict_label([subgraph], [graph])
+    results = model.predict_label([subgraph], [graph])
     print("result", results[0] > args.confidence)
 
     # if results[0] > args.confidence:
     if True:
-        interactions = inference_gnn.predict_embedding([subgraph], [graph])
+        interactions = model.predict_embedding([subgraph], [graph])
         # print("interactions", interactions[0])
         n_subgraph_atom = subgraph.number_of_nodes()
         x_coord, y_coord = np.where(interactions[0] > args.mapping_threshold)
@@ -209,36 +249,29 @@ if __name__ == "__main__":
         interaction_dict = {}
         for x, y in zip(x_coord, y_coord):
             if x < n_subgraph_atom and y >= n_subgraph_atom:
-                interaction_dict[(x, y - n_subgraph_atom)] = interactions[0][x][y]
+                interaction_dict[(x, y-n_subgraph_atom)
+                                 ] = interactions[0][x][y]
                 # print("(", x, y-n_ligand_atom, ")")
 
-            if (
-                x >= n_subgraph_atom
-                and y < n_subgraph_atom
-                and (y, x - n_subgraph_atom) not in interaction_dict
-            ):
-                interaction_dict[(y, x - n_subgraph_atom)] = interactions[0][x][y]
+            if x >= n_subgraph_atom and y < n_subgraph_atom and (y, x-n_subgraph_atom) not in interaction_dict:
+                interaction_dict[(y, x-n_subgraph_atom)
+                                 ] = interactions[0][x][y]
                 # print("(", y, x-n_ligand_atom, ")")
 
         list_mapping = list(interaction_dict.keys())
         mapping_dict = {}
         for node in subgraph.nodes:
-            cnode_mapping = list(
-                map(
-                    lambda y: (y[1], interaction_dict[y]),
-                    filter(lambda x: x[0] == node, list_mapping),
-                )
-            )
+            cnode_mapping = list(map(lambda y: (y[1], interaction_dict[y]), filter(
+                lambda x: x[0] == node, list_mapping)))
             if len(cnode_mapping) == 0:
                 mapping_dict[node] = []
                 continue
 
             max_prob = max(cnode_mapping, key=lambda x: x[1])[1]
-            mapping_dict[node] = list(
-                map(lambda x: x[0], filter(lambda y: y[1] == max_prob, cnode_mapping))
-            )
+            mapping_dict[node] = list(map(lambda x: x[0], filter(
+                lambda y: y[1] == max_prob, cnode_mapping)))
 
-        print(mapping_dict)
+        # print(mapping_dict)
 
         node_labels = {n: "" for n in graph.nodes}
         for sgn, list_gn in mapping_dict.items():
@@ -270,10 +303,8 @@ if __name__ == "__main__":
         edge_colors = {n: "whitesmoke" for n in graph.edges}
         for edge in graph.edges:
             n1, n2 = edge
-            n1_sgs, n2_sgs = (
-                node_labels[n1],
-                node_labels[n2],
-            )  # map node from graph to node in subgraph
+            # map node from graph to node in subgraph
+            n1_sgs, n2_sgs = node_labels[n1], node_labels[n2]
 
             if node_colors[n1] == "gray" or node_colors[n2] == "gray":
                 continue
@@ -285,10 +316,7 @@ if __name__ == "__main__":
                 n1_sg = int(n1_sg)
                 for n2_sg in n2_sgs.split(","):
                     n2_sg = int(n2_sg)
-                    if (n1_sg, n2_sg) not in subgraph.edges and (
-                        n2_sg,
-                        n1_sg,
-                    ) not in subgraph.edges:
+                    if (n1_sg, n2_sg) not in subgraph.edges and (n2_sg, n1_sg) not in subgraph.edges:
                         count_pair += 1
 
             if count_pair != total_pair:
@@ -302,18 +330,11 @@ if __name__ == "__main__":
                 if node_colors[n1] == "pink" or node_colors[n2] == "pink":
                     edge_colors[edge] = "palevioletred"
 
-        utils.plotGraph(
-            graph,
-            nodeLabels=node_labels,
-            nodeColors=list(node_colors.values()),
-            edgeColors=list(edge_colors.values()),
-        )
+        utils.plotGraph(graph, nodeLabels=node_labels,
+                        nodeColors=list(node_colors.values()),
+                        edgeColors=list(edge_colors.values()))
 
-        with open(
-            "results/mapping_%s.csv" % datetime.now().strftime("%Y-%m-%dT%H-%M-%S"),
-            "w",
-            encoding="utf8",
-        ) as f:
+        with open(f"{args.result_dir}/mapping_{args.source}_{args.query}.csv", "w", encoding="utf8") as f:
             f.write("subgraph_node,graph_node,score\n")
             for key, value in interaction_dict.items():
                 f.write("{:d},{:d},{:.3e}\n".format(key[0], key[1], value))
